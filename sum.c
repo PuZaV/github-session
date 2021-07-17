bd9163dcaaaddb77477e7f070a84573d0e2eabc9
puza <lakhepuza@gmail.com>
2021-07-17 21:34:07 +0545

    count is finished

---
+++ sum.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+ //sum program 
+ int a,b,sum=0;
+ a=5;
+ b=2;
+ sum=a+b;
+ printf("sum of the two numbers is %d\n",sum);
+//count
+for(int i=1;i<=10;i++)
+{
+	
+	printf("%d\n",i);
+}
+ 
+
+
+    return 0;
+}
