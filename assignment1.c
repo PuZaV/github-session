3b197ea6a0c4ea3d289fa9b74fc63f6322413e6e
puza <lakhepuza@gmail.com>
2021-07-17 22:25:43 +0545

    switch is also finished

--- assignment1.c
+++ assignment1.c
@@ -1,20 +1,30 @@
 #include<stdio.h>
 int main()
 {
- //sum program 
- int a,b,sum=0;
- a=5;
- b=2;
- sum=a+b;
- printf("sum of the two numbers is %d\n",sum);
-//count
-for(int i=1;i<=10;i++)
-{
-	
-	printf("%d\n",i);
-}
- 
-
-
+	int ans;
+	int a,b,sum=0;
+	printf("choose a program\n");
+	printf("1.sum of two numbers\n");
+	printf("2.count to 10\n");
+	printf("answer;\n");
+	scanf("%d", &ans);
+	switch(ans)
+	{
+		case 1:
+        a=5;
+        b=2;
+        sum=a+b;
+        printf("sum of the two numbers is %d\n",sum);
+		break;
+		case 2:
+	   for(int i=1;i<=10;i++)
+       {
+	      printf("%d\n",i);
+       }
+       break;
+	   default :
+	   printf("invalid input\n");
+	   	
+	}
     return 0;
 }
